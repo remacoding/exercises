f55b144aea403fb8af956a04b9c195ed9d31e803
remacoding <105727870+remacoding@users.noreply.github.com>
2023-02-07 22:34:59 +0200

    Exercise 3

---
+++ variable.php
@@ -0,0 +1,10 @@
+<?php
+include "header.php" ?>
+
+<h3>Exercise 3. In-class Task Variable & Operators 07.02.2023 (variable.php)</h3>
+
+<h2>Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print using
+    tag: Hello …., You are welcome to my site.</h2>
+
+
+<?php include "footer.php" ?>
