fb258ea34e2f2c93e3de1729fe646e08f6da377c
remacoding <105727870+remacoding@users.noreply.github.com>
2023-02-07 23:57:26 +0200

    Exercise 3 update

--- variable.php
+++ variable.php
@@ -1,16 +1,28 @@
-<?php
-include "header.php" ?>
+<?php include "header.php" ?>
 
 <h3>Exercise 3. In-class Task Variable & Operators 07.02.2023 (variable.php)</h3>
 
 <h2>Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print using
     tag: Hello …., You are welcome to my site.</h2>
 
-<form action="">
+<form action="action.php" method="post">
+    <div class="row">
 
-First Name: <input type="text" name="fname"> <br>
-Last Name: <input type="text" name="lname"> <br>
-<input type="submit" value="Submit">
+        <div class="col">
+            <input type="text" name="fname" required placeholder="First name" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" name="lname" required placeholder="Last name" class="form-control">
+        </div>
+        <div class="col">
+        Birth Date: <input type="date" name="bdate" required placeholder="Birth Date" class="form-control">
+        </div>
+        <div class="col">
+        Select fav Color: <input type="color" name="color" required placeholder="Select fav color" class="form-control">
+        </div>
+
+        <input type="submit" value="Submit">
+    </div>
 
 </form>
 
