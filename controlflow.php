e5345ba0b61dbf1db1ee9d46e7b4d8f695645661
remacoding <105727870+remacoding@users.noreply.github.com>
2023-02-08 20:02:18 +0200

    Exercise 3 completed

--- controlflow.php
+++ controlflow.php
@@ -1,4 +1,6 @@
-<?php include "header.php" ?>
+<?php 
+$title = "Exercise 3";
+include "header.php" ?>
 
 
 <h3>1. Write a script that gets the current month and prints one of the following responses, depending on whether it's
